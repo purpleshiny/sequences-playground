aec64fad8a12737b44e7a40c70a47a846f4dd50f
chawkes-home <chawkes-home@Christinas-MacBook-Pro.local>
2019-11-06 19:47:55 -0500

    Updates

---
+++ precache-manifest.807e24b7bcf2cb008e3db358f5fddd7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec345e53d2c3dbb410b4c82cebee58c",
+    "url": "/sequences-playground/index.html"
+  },
+  {
+    "revision": "9f434a7d25dc63903f9f",
+    "url": "/sequences-playground/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3060f6bf16d16564ea5e",
+    "url": "/sequences-playground/static/js/2.aebb6e27.chunk.js"
+  },
+  {
+    "revision": "9f434a7d25dc63903f9f",
+    "url": "/sequences-playground/static/js/main.c9327ede.chunk.js"
+  },
+  {
+    "revision": "66985ad3dd3bffd8098b",
+    "url": "/sequences-playground/static/js/runtime-main.fb25ef9a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/sequences-playground/static/media/logo.25bf045c.svg"
+  }
+]);
